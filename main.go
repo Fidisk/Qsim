df6ec3bf2de7a9c2581466efaa3804d1d490baaa
Fidisk <70708171+Fidisk@users.noreply.github.com>
2026-06-02 23:48:52 +0700

    ci: test workflow failure handling
    Verify the workflow detects build errors.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	comp "qsim/components"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
@@ -12,6 +13,8 @@ import (
 
 func main() {
 	InitMainWindow(800, 600, "Floating Panels")
+
+	fmt.Println("Hey there")
 
 	defer rl.CloseWindow()
 	rl.SetTargetFPS(60)
