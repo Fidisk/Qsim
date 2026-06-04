d95c021c687d32a3ec6ec827375e3941ad842a0c
Fidisk <70708171+Fidisk@users.noreply.github.com>
2026-06-04 22:00:36 +0700

    feat: add physics to circle component
    Add basic physics behavior (velocity, acceleration) to circles.

--- components/circle.go
+++ components/circle.go
@@ -1,6 +1,8 @@
 package components
 
 import (
+	"qsim/globals"
+
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
@@ -11,6 +13,8 @@ type Circle struct {
 	Color    rl.Color
 	dragging bool
 	offset   rl.Vector2
+	curForce rl.Vector2
+	weight   float32
 }
 
 func NewCircle(x, y, radius float32, color rl.Color) *Circle {
@@ -39,4 +43,32 @@ func (c *Circle) Update(worldMouse rl.Vector2, holdingCursor bool, isCursorAvail
 
 func (c *Circle) Draw() {
 	rl.DrawCircleV(c.Center, c.Radius, c.Color)
+}
+
+func (c *Circle) ApplyForce() {
+	c.Center.Add(c.curForce)
+}
+
+func (c *Circle) AddForce(force rl.Vector2) {
+	c.curForce.Add(force)
+}
+
+func (c *Circle) DecayForce() {
+	//Dumb ass way to do friction
+	c.curForce.Scale(globals.ForceDecay)
+}
+
+// AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+func (c *Circle) AntiGravity(d *Circle) {
+	//Another dumb ass way, non Newtonian
+	cmpWeight := c.weight * d.weight
+	tmp := d.Center.Subtract(c.Center).ClampValue(1, 1000000) //Magic Number
+	c.AddForce(rl.Vector2{X: cmpWeight / tmp.X, Y: cmpWeight / tmp.Y})
+}
+
+func (c *Circle) Gravity(d *Circle) {
+	//Hi there, Daniel
+	cmpWeight := c.weight * d.weight
+	tmp := d.Center.Subtract(c.Center).ClampValue(1, 10)
+	c.AddForce(rl.Vector2{X: -cmpWeight / tmp.X, Y: -cmpWeight / tmp.Y})
 }
